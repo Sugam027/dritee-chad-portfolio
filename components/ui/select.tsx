ab808ce6db05a009bf600804796d756672a66c10
sugamXrestha <120570437+sugamXrestha@users.noreply.github.com>
2025-12-12 16:16:44 +0545

    publication, blog pages added

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -65,7 +65,7 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md " +
+        "relative z-50 max-h-96 min-w-32 overflow-hidden rounded-md bg-background text-popover-foreground shadow-md " +
           "data-[state=open]:animate-in data-[state=closed]:animate-out " +
           "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 " +
           "data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 " +
@@ -83,7 +83,7 @@ const SelectContent = React.forwardRef<
         className={cn(
           "p-1",
           position === "popper" &&
-            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]"
+            "h-(--radix-select-trigger-height) w-full min-w-(--radix-select-trigger-width)"
         )}
       >
         {children}
@@ -114,7 +114,7 @@ const SelectItem = React.forwardRef<
     ref={ref}
     className={cn(
       "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none " +
-        "focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+        "focus:bg-primary focus:text-muted data-disabled:pointer-events-none data-disabled:opacity-50",
       className
     )}
     {...props}
